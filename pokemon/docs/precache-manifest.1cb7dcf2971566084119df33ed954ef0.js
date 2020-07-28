580988b911919034057e884e5d483614983483bc
Corina <cborcoci@gmail.com>
2020-07-28 09:41:12 +0200

    run docs

---
+++ pokemon/docs/precache-manifest.1cb7dcf2971566084119df33ed954ef0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3de32eae6a8b611517cfbc86f99d3a0b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b033b17b7a6437bbb71a",
+    "url": "./static/css/main.0bad6a70.chunk.css"
+  },
+  {
+    "revision": "0f90abc7bb0e0596206f",
+    "url": "./static/js/2.d20fa86a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d20fa86a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b033b17b7a6437bbb71a",
+    "url": "./static/js/main.c6a53dd0.chunk.js"
+  },
+  {
+    "revision": "118a6eeef37089d48409",
+    "url": "./static/js/runtime-main.c0891a34.js"
+  }
+]);
